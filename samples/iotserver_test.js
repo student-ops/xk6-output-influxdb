d50970d75ec6ed8a49179bfdadad27bbbf837ff8
student-ops <gogakuec@gmail.com>
2024-01-31 14:20:11 +0900

    update my  test script

--- samples/iotserver_test.js
+++ samples/iotserver_test.js
@@ -4,9 +4,12 @@ import http from 'k6/http';
 export default function () {
     const url = 'http://localhost:8080/handle';
 
-    let tmp = 25; // 温度
-    let moi = 65; // 湿度
-    let air = 1019; // 気圧
+    // let tmp = 25; // 温度
+    // let moi = 65; // 湿度
+    // let air = 1019; // 気圧
+    let tmp = 20 + 5 - 10 * Math.random();
+    let moi = 60 + 5 - 10 * Math.random();
+    let air = 1024 + 5 - 10 * Math.random();
 
     const data = {
         "surroundings": [
